ce5a7010276c74a8e8f750f09527c8d81f9aed33
ameCont <americanCont7@gmail.com>
2020-06-02 09:00:24 -0400

    min33:51

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,10 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
     app.post('/register',
+        AuthenticationController.register,
         AuthenticationController.register)
         console.log('messageRouts')
 }
