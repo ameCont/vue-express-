d0cd19be092b9a73da0bdf166b01b1bd2110a259
ameCont <americanCont7@gmail.com>
2020-06-02 11:30:31 -0400

    still issues

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -3,10 +3,11 @@ const {User} = require('../models')
 module.exports = {
     async register (req, res) {
         try {
-          const user = await User.create(req,body)
+          const user = await User.create(req.body)
+          res.send(user.toJSON())
         } catch(err) {
             res.status(400).send({
-                error: 'This email account is already in use.'
+              error: 'This email account is already in use.'
             })
           }
     }
--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -5,7 +5,7 @@ module.exports = {
         const schema = {
             email: Joi.string().email(),
             password: Joi.string().regex(
-                new RegExp['^[a-zA-Z0-9]{8,32}$']
+                new RegExp('^[a-zA-Z0-9]{8,32}$')
             )
         }
         const {error, value} = Joi.validate(req.body, schema)
@@ -20,9 +20,9 @@ module.exports = {
                     res.status(400).send({
                         error: `The password provided failed to match the following rules:
                         <br>
-                        1. It must contain ONLY the following characters: lowe case, upper case
+                        1. It must contain ONLY the following characters: lowe case, upper case, numerics
                         <br>
-                        2. It must be at least 8 characters in length and not greater`
+                        2. It must be at least 8 characters in length and not greater than 32 characters in length`
 
                     })
                     break
