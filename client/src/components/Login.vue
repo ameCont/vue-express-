1d37583521a00de740963e7ee1e893c605128bb8
ameCont <americanCont7@gmail.com>
2020-06-04 14:23:07 -0400

    500internal error

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -48,8 +48,8 @@ export default {
           email: this.email,
           password: this.password
         })
-        this.$store.dispatch('setToken', response.token)
-        this.$store.dispatch('setUser', response.user)
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
       } catch (error) {
         this.error = error.response.data.error
       }
