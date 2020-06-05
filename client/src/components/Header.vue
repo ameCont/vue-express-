3ee198735aba5ff5b7de1f1b78fa235ecc95309d
ameCont <americanCont7@gmail.com>
2020-06-05 10:53:30 -0400

    min 13  32intoarceri

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -8,16 +8,24 @@
       </span>
     </v-toolbar-title>
 
-    <!--  todo: implement me
       <v-toolbar-items>
-      <v-btn flat dark>
+        <v-btn
+        flat
+        dark
+        @click="navigateTo({name: 'songs'})">
         Browse
-      </v-btn>
-    </v-toolbar-items> -->
+        </v-btn>
+    </v-toolbar-items>
 
     <v-spacer></v-spacer>
 
     <v-toolbar-items>
+      <v-btn
+      v-if="!$store.state.isUserLoggedIn"
+      flat dark
+      @click="navigateTo({name: 'login'})">
+      Login
+      </v-btn>
       <v-btn
       v-if="!$store.state.isUserLoggedIn"
       flat dark
@@ -31,6 +39,12 @@
       @click="navigateTo({name: 'register'})">
         Sign Up
       </v-btn>
+      <v-btn
+      v-if="$store.state.isUserLoggedIn"
+      flat dark
+      @click="logout">
+        Log Out
+      </v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -41,6 +55,13 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
     }
   }
 }
---
+++ client/src/components/Songs.vue
@@ -0,0 +1,32 @@
+<template>
+   <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <panel title="Songs">
+        <!-- <div slot="hello">
+          <h3>YO YO YO</h3>
+          <p>Hello there I am a paragraph tag</p>
+        </div>
+        <div slot="bye">
+          <h3>bye bye bye</h3>
+          <p>
+            BYE
+          </p>
+        </div>
+        -->
+      </panel>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+export default {
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
