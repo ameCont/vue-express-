1c268896bfae0499cb0dc2d04acbb1c21958ef8a
ameCont <americanCont7@gmail.com>
2020-06-08 16:04:46 -0400

    part4 final

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -15,10 +15,41 @@
           <v-icon>add</v-icon>
         </v-btn>
         <div v-for="song in songs"
+        class="song"
         :key="song.id">
-          {{song.title}} -
-          {{song.artist}} -
-          {{song.album}}
+
+        <v-layout>
+          <v-flex xs6>
+            <div class="song-title">
+              {{song.title}} -
+            </div>
+
+            <div class="song-artist">
+              {{song.artist}} -
+            </div>
+
+            <div class="song-genre">
+              {{song.genre}}
+            </div>
+
+          <v-btn
+            dark
+            class="cyan"
+            @click="navigateTo({
+              name: 'song',
+              params: {
+                songId: song.id
+              }
+            })">
+            View
+          </v-btn>
+
+          </v-flex>
+
+          <v-flex xs6>
+            <img class="album-image" :src="song.albumImageUrl"/>
+          </v-flex>
+        </v-layout>
         </div>
       </panel>
     </v-flex>
@@ -52,5 +83,26 @@ export default {
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
 
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
