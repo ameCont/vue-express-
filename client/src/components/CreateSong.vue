077a2a6889d26e09d0ef1d2c13e740a1f701da03
ameCont <americanCont7@gmail.com>
2020-06-08 13:04:54 -0400

    part 4 o ora

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -4,31 +4,43 @@
       <panel title="Song Metadata">
         <v-text-field
         label="Title"
+        required
+        :rules="[required]"
         v-model="song.title">
         </v-text-field>
 
         <v-text-field
         label="Artist"
+        required
+        :rules="[required]"
         v-model="song.artist">
         </v-text-field>
 
         <v-text-field
         label="Genre"
+        required
+        :rules="[required]"
         v-model="song.genre">
         </v-text-field>
 
         <v-text-field
         label="Album"
+        required
+        :rules="[required]"
         v-model="song.album">
         </v-text-field>
 
         <v-text-field
         label="Album Image URL"
+        required
+        :rules="[required]"
         v-model="song.albumImageUrl">
         </v-text-field>
 
         <v-text-field
         label="Youtube ID"
+        required
+        :rules="[required]"
         v-model="song.youtubeId">
         </v-text-field>
       </panel>
@@ -38,23 +50,31 @@
       <panel title="Song Structure" class="ml-2">
         <v-text-field
             label="Tab"
+            required
+            :rules="[required]"
             multi-line
             v-model="song.tab">
         </v-text-field>
 
         <v-text-field
             label="Lyrics"
+            required
+            :rules="[required]"
             multi-line
             v-model="song.lyrics">
         </v-text-field>
       </panel>
 
-       <v-btn
-          dark
-          class="cyan"
-          @click="create">
-          Create Song
-        </v-btn>
+      <span class="error" v-if="error">
+        {{error}}
+      </span>
+
+      <v-btn
+        dark
+        class="cyan"
+        @click="create">
+        Create Song
+       </v-btn>
     </v-flex>
   </v-layout>
 </template>
@@ -74,11 +94,21 @@ export default {
         youtubeId: null,
         lyrics: null,
         tab: null
-      }
+      },
+      error: null,
+      required: (value) => !!value || 'Required'
     }
   },
   methods: {
     async create () {
+      this.error = null
+      const areAllFieldsFilledIn = Object
+      .keys(this.song)
+      .every(key => !!song[key])
+    if (!areAllFieldsFilledIn) {
+      this.error = 'Please fill in all the required fields'
+      return
+    }
       try {
         await SongsService.post(this.song)
         this.$router.push({
