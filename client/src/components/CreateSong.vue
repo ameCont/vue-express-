73b59803860c064b890b3525ee879f98e422238b
ameCont <americanCont7@gmail.com>
2020-06-08 12:37:55 -0400

    53m

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -4,70 +4,92 @@
       <panel title="Song Metadata">
         <v-text-field
         label="Title"
-        v-model="title">
+        v-model="song.title">
         </v-text-field>
 
         <v-text-field
         label="Artist"
-        v-model="artist">
+        v-model="song.artist">
         </v-text-field>
 
         <v-text-field
         label="Genre"
-        v-model="genre">
+        v-model="song.genre">
         </v-text-field>
 
         <v-text-field
         label="Album"
-        v-model="album">
+        v-model="song.album">
         </v-text-field>
 
         <v-text-field
         label="Album Image URL"
-        v-model="albumImageUrl">
+        v-model="song.albumImageUrl">
         </v-text-field>
 
         <v-text-field
         label="Youtube ID"
-        v-model="youtubeId">
+        v-model="song.youtubeId">
         </v-text-field>
       </panel>
     </v-flex>
 
     <v-flex xs8>
-      <panel title="Song Structure">
+      <panel title="Song Structure" class="ml-2">
         <v-text-field
             label="Tab"
-            multiline
-            v-model="tab">
+            multi-line
+            v-model="song.tab">
         </v-text-field>
 
         <v-text-field
             label="Lyrics"
-            multiline
-            v-model="lyrics">
+            multi-line
+            v-model="song.lyrics">
         </v-text-field>
       </panel>
+
+       <v-btn
+          dark
+          class="cyan"
+          @click="create">
+          Create Song
+        </v-btn>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import Panel from '@/components/Panel'
-
+import SongsService from '@/services/SongsService'
 export default {
   data () {
     return {
-      title: null,
-      artist: null,
-      genre: null,
-      album: null,
-      albumImageUrl: null,
-      youtubeId: null,
-      lyrics: null,
-      tab: null
+      song: {
+        title: null,
+        artist: null,
+        genre: null,
+        album: null,
+        albumImageUrl: null,
+        youtubeId: null,
+        lyrics: null,
+        tab: null
+      }
     }
   },
+  methods: {
+    async create () {
+      try {
+        await SongsService.post(this.song)
+        this.$router.push({
+          name: 'songs'
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+
   components: {
     Panel
   }
