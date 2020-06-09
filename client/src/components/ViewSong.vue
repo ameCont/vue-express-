b11c7f731c286a36aceef59dc1ce3d4b8c2a9a03
ameCont <americanCont7@gmail.com>
2020-06-09 10:19:52 -0400

    P4 M18:37

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -28,7 +28,10 @@
 
     <v-flex xs6 class="ml-2">
       <panel title="Tabs">
-        {{song.tab}}
+        <textarea
+          readonly
+          v-model="song.tab">
+        </textarea>
       </panel>
     </v-flex>
   </v-layout>
@@ -78,5 +81,16 @@ export default {
 .album-image {
   width: 70%;
   margin: 0 auto;
+}
+
+textarea {
+  width: 100%;
+  font-family: monospace;
+  border: none;
+  height: 600px;
+  border-style: none;
+  border-color: transparent;
+  overflow: auto;
+  padding: 40px;
 }
 </style>
