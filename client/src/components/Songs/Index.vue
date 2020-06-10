59f1c1f5ae975f08d452472b747261a12e06dc86
ameCont <americanCont7@gmail.com>
2020-06-10 06:01:14 -0400

    P5 Min4

---
+++ client/src/components/Songs/Index.vue
@@ -0,0 +1,63 @@
+<template>
+   <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <songs-search-panel />
+      <songs-panel />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import SongsPanel from './SongsPanel'
+import SongsSearchPanel from './SongsSearchPanel'
+import SongsService from '@/services/SongsService'
+import Panel from '@/components/Panel'
+export default {
+  components: {
+    Panel,
+    SongsPanel,
+    SongsSearchPanel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
+  async mounted () {
+  // mounted () {
+    this.songs = (await SongsService.index()).data
+    // this.songs = (SongsService.index()).data
+    console.log('songs', this.songs)
+  }
+}
+</script>
+
+<style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
+</style>
