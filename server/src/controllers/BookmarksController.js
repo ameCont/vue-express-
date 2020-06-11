531a6b0a23a54d3cf39925b99aeb3d06fd0f1b77
ameCont <americanCont7@gmail.com>
2020-06-11 06:30:49 -0400

    p6 m1:06:59

--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -21,8 +21,10 @@ module.exports = {
     },
     async post (req, res) {
       try {
-        const bookmark = req.body
-        await Bookmark.create(bookmark)
+        const {songId, userId} = req.body
+        const bookmark = await Bookmark.findOne({})
+
+        const newBookmark = await Bookmark.create(req.body)
         res.send(bookmark)
       } catch (err) {
         res.status(500).send({
