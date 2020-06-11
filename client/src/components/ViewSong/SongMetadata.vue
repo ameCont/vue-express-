b8b1f08548358ba208fdc1e351f673fc487377d3
ameCont <americanCont7@gmail.com>
2020-06-11 05:09:29 -0400

    min53 error in mounted hook

--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -2,49 +2,86 @@
     <panel title="Song Metadata">
         <v-layout>
         <v-flex xs6>
-            <div class="song-title">
-            {{song.title}} -
-            </div>
+          <div class="song-title">
+          {{song.title}} -
+          </div>
 
-            <div class="song-artist">
-            {{song.artist}} -
-            </div>
+          <div class="song-artist">
+          {{song.artist}} -
+          </div>
 
-            <div class="song-genre">
-            {{song.genre}}
-            </div>
+          <div class="song-genre">
+          {{song.genre}}
+          </div>
 
-            <v-btn
-            dark
-            class="cyan"
-            :to="{
-              name: 'songs-edit',
-              params () {
-                return {
-                  songId: song.id
-                }
+          <v-btn
+          dark
+          class="cyan"
+          :to="{
+            name: 'songs-edit',
+            params () {
+              return {
+                songId: song.id
               }
-            }">
-            Edit
-          </v-btn>
+            }
+          }">
+          Edit
+        </v-btn>
 
+        <v-btn
+          v-if="isUserLoggedIn"
+          dark
+          class="cyan"
+          @click="unbookmark">
+          Bookmark
+        </v-btn>
+
+        <v-btn
+          v-if="isUserLoggedIn"
+          dark
+          class="cyan"
+          @click="bookmark">
+          Unbookmark
+        </v-btn>
         </v-flex>
 
         <v-flex xs6>
-            <img class="album-image" :src="song.albumImageUrl"/>
-            <br>
-            {{song.album}}
+          <img class="album-image" :src="song.albumImageUrl"/>
+          <br>
+          {{song.album}}
         </v-flex>
-        </v-layout>
+      </v-layout>
     </panel>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+import BookmarksService from '@/services/BookmarksService'
 
 export default {
   props: [
     'song'
-  ]
+  ],
+  computed: {
+    ...mapState([
+      'isUserLoggedIn'
+    ])
+  },
+  async mounted () {
+    const bookmark = await BookmarksService.index({
+      songId: 1,
+      userId: 1
+    })
+    console.log('bookmark', bookmark)
+  },
+  methods: {
+    bookmark () {
+      console.log('bookmark')
+    },
+    unbookmark () {
+      console.log('unbookmark')
+    }
+  }
 }
 </script>
 
