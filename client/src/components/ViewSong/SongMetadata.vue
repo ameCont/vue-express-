0e6c5c0a13ed5e30157b695491c57b4cf0d850cb
ameCont <americanCont7@gmail.com>
2020-06-11 05:52:08 -0400

    m58:31

--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -73,19 +73,37 @@ export default {
     ])
   },
   async mounted () {
-    const bookmark = (await BookmarksService.index({
-      songId: 1,
-      userId: 1
-    })).data
-    this.isBookmarked = !!bookmark
-    console.log('bookmark', this.isBookmarked)
+    try {
+      const bookmark = (await BookmarksService.index({
+        songId: this.song.id,
+        userId: this.$store.state.user.id
+      })).data
+      this.isBookmarked = !!bookmark
+      console.log('bookmark', this.isBookmarked)
+    } catch (err) {
+      console.log(err)
+    }
   },
   methods: {
-    bookmark () {
-      console.log('bookmark')
+    async bookmark () {
+      try {
+        await BookmarksService.post({
+          songId: this.song.id,
+          userId: this.$store.state.user.id
+        })
+      } catch (err) {
+          console.log(err)
+      }
     },
-    unbookmark () {
-      console.log('unbookmark')
+    async unbookmark () {
+      try {
+        await BookmarksService.delete({
+          songId: this.song.id,
+          userId: this.$store.state.user.id
+        })
+      } catch (err) {
+          console.log(err)
+      }
     }
   }
 }
