bfaeb4d577881f3544e19b4266c9093c7da699d2
ameCont <americanCont7@gmail.com>
2020-06-11 04:43:19 -0400

    m47

--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -1,31 +1,18 @@
 const {Bookmark} = require('../models')
-const { Op } = require('sequelize');
 
 module.exports = {
     async index (req, res) {
       try {
-        let songs = null
-        const search = req.query.search
-        if (search) {
-          songs = await Song.findAll({
-            where: {
-              [Op.or]: [
-                'title', 'artist', 'genre', 'album'
-              ].map(key => ({
-                [key]: {
-                  [Op.like]: `%${search}%`
-                }
-              }))
-            }
-          })
-        } else {
-          songs = await Song.findAll({
-            // const songs = Song.findAll({
-            limit: 10
-          })
-        }
-        res.send(songs)
-        console.log("we are at index")
+        const {songId, userId} = req.query
+        const bookmark = await Bookmark.findOne({
+          where: {
+            SongId: songId,
+            UserId: userId
+          }
+        })
+        console.log('bookmark', bookmark)
+        res.send(bookmark)
+        console.log("we are at bookmark")
       } catch (err) {
         res.status(500).send({
           error: 'an error has occured trying to fetch the songs'
